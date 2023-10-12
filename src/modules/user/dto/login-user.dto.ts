4845524f764beba074b087d551151eaa310239ef
luc <luc.bachelerieart@gmail.com>
2023-10-12 23:28:19 +0200

    add constraint of password length &  character

--- src/modules/user/dto/login-user.dto.ts
+++ src/modules/user/dto/login-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsLowercase, IsString, IsNotEmpty } from 'class-validator';
+import { Matches, IsString, MinLength, IsNotEmpty } from 'class-validator';
 
 export class LoginUserDto {
   @IsString()
@@ -7,5 +7,12 @@ export class LoginUserDto {
 
   @IsString()
   @IsNotEmpty()
+  @MinLength(8, {
+    message: 'Le mot de passe doit contenir au moins 8 caractères.',
+  })
+  @Matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).+/, {
+    message:
+      'Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial.',
+  })
   password: string;
 }
